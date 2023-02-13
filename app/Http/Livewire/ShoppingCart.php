f37f0dbc3b95884fe0e0de54c99fb39135c4c4b0
JesusMedinaP <jesusmedinaparraga@gmail.com>
2023-02-13 20:45:49 +0100

    Fin Video 78. PDF 15

--- app/Http/Livewire/ShoppingCart.php
+++ app/Http/Livewire/ShoppingCart.php
@@ -6,6 +6,9 @@ use Livewire\Component;
 
 class ShoppingCart extends Component
 {
+
+    public $listeners = ['render'];
+
     public function render()
     {
         return view('livewire.shopping-cart');
