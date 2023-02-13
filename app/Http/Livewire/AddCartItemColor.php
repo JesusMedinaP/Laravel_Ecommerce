e90e7afdaf7963e727b6d65c3352bf95388fd37f
JesusMedinaP <jesusmedinaparraga@gmail.com>
2023-02-13 13:23:38 +0100

    Fin Video 50. Fin PDF 10

--- app/Http/Livewire/AddCartItemColor.php
+++ app/Http/Livewire/AddCartItemColor.php
@@ -8,6 +8,24 @@ class AddCartItemColor extends Component
 {
     public $product;
     public $colors;
+    public $qty = 1;
+    public $quantity = 0;
+    public $color_id = '';
+
+
+    public function updatedColorId($value)
+    {
+        $this->quantity = $this->product->colors->find($value)->pivot->quantity;
+    }
+
+    public function decrement()
+    {
+        $this->qty--;
+    }
+    public function increment()
+    {
+        $this->qty++;
+    }
 
     public function mount()
     {
