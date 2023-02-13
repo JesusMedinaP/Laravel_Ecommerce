3be3ae1e8d8191940affd3da35bde80e2f1b0c5d
JesusMedinaP <jesusmedinaparraga@gmail.com>
2023-02-13 13:04:01 +0100

    Fin Video 49. PDF 10

--- resources/views/livewire/add-cart-item-color.blade.php
+++ resources/views/livewire/add-cart-item-color.blade.php
@@ -1,3 +1,7 @@
-<div>
-    {{-- Stop trying to control. --}}
-</div>
+<p class="text-xl text-gray-700">Color:</p>
+<select class="form-control w-full">
+    <option value="" selected disabled>Seleccionar un color</option>
+    @foreach ($colors as $color)
+        <option value="{{$color->id}}">{{ $color->name }}</option>
+    @endforeach
+</select>
