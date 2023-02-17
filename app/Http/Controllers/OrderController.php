d25c9299e1caa157cbc7ff00a924e6e3a7685b94
JesusMedinaP <jesusmedinaparraga@gmail.com>
2023-02-17 20:26:18 +0100

    Fin Video 110. Fin PDF 20.

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -9,15 +9,11 @@ class OrderController extends Controller
 {
 
 
-    public function payment(Order $order)
-    {
-        $items = json_decode($order->content);
-
-        return view('orders.payment', compact('order', 'items'));
-    }
-
     public function show(Order $order)
     {
-        return view('orders.show', compact('order'));
+
+        $items = json_decode($order->content);
+
+        return view('orders.show', compact('order', 'items'));
     }
 }
