e0ca6c01f723e54253b44ca68b3d87c81e1d9f04
JesusMedinaP <jesusmedinaparraga@gmail.com>
2023-02-22 20:29:28 +0100

    Fin Video 168. Fin PDF 26.

--- app/Http/Livewire/CategoryFilter.php
+++ app/Http/Livewire/CategoryFilter.php
@@ -13,6 +13,7 @@ class CategoryFilter extends Component
 
     public $category, $subcategoria, $marca;
     public $view = 'grid';
+    public $queryString = ['subcategoria', 'marca'];
 
     public function render()
     {
@@ -21,7 +22,7 @@ class CategoryFilter extends Component
         });
         if ($this->subcategoria) {
             $productsQuery = $productsQuery->whereHas('subcategory', function(Builder $query){
-                $query->where('name', $this->subcategoria);
+                $query->where('slug', $this->subcategoria);
             });
         }
         if ($this->marca) {
@@ -33,8 +34,18 @@ class CategoryFilter extends Component
         return view('livewire.category-filter', compact('products'));
     }
 
+    public function updatedSubcategoria()
+    {
+        $this->resetPage();
+    }
+
+    public function updatedMarca()
+    {
+        $this->resetPage();
+    }
+
     public function limpiar()
     {
-        $this->reset(['subcategoria', 'marca']);
+        $this->reset(['subcategoria', 'marca', 'page']);
     }
 }
