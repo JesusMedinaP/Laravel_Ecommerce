99a8ca5855fc630b5bf8dda20680aa37bd76ea5d
JesusMedinaP <jesusmedinaparraga@gmail.com>
2023-03-07 19:57:42 +0100

    Arreglos varios

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -25,39 +25,70 @@
                 @livewire('search')
             </div>
 
+
+            <!-- Settings Dropdown -->
             <div class="mx-6 relative hidden md:block">
-                @auth
-                <x-jet-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <i class="fas fa-user-circle text-white text-3xl cursor-pointer"></i>
+
+                    @auth
+                    <x-jet-dropdown align="right" width="48">
+
+                        <x-slot name="trigger">
                             <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                                <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                                <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name}}" />
                             </button>
-                    </x-slot>
+                        </x-slot>
 
-                    <x-slot name="content">
+                        <x-slot name="content">
+                            <!-- Account Management -->
+                            <div class="block px-4 py-2 text-xs text-gray-400">
+                                {{ __('Manage Account') }}
+                            </div>
 
-                        <x-jet-dropdown-link href="{{ route('login') }}">
-                            {{ __('Login') }}
-                        </x-jet-dropdown-link>
+                            <x-jet-dropdown-link href="{{ route('profile.show') }}">
+                                {{ __('Profile') }}
+                            </x-jet-dropdown-link>
 
-                        <x-jet-dropdown-link href="{{ route('register') }}">
-                            {{ __('Register') }}
-                        </x-jet-dropdown-link>
+                            <div class="border-t border-gray-100"></div>
 
-                        <x-jet-dropdown-link href="{{ route('orders.index') }}">
-                            {{ __('My Orders') }}
-                        </x-jet-dropdown-link>
+                            <!-- Authentication -->
+                            <form method="POST" action="{{ route('logout') }}" x-data>
+                                @csrf
 
-                        @role('admin')
-                        <x-jet-dropdown-link href="{{ route('admin.index') }}">
-                            {{ __('Admin') }}
-                        </x-jet-dropdown-link>
-                        @endrole
+                                <x-jet-dropdown-link href="{{ route('logout') }}"
+                                                     @click.prevent="$root.submit();">
+                                    {{ __('Log Out') }}
+                                </x-jet-dropdown-link>
 
-                    </x-slot>
-                </x-jet-dropdown>
-                @endauth
+                                <x-jet-dropdown-link href="{{ route('orders.index') }}">
+                                    {{ __('My Orders') }}
+                                </x-jet-dropdown-link>
+                                @role('admin')
+                                <x-jet-dropdown-link href="{{ route('admin.index') }}">
+                                    {{ __('Admin') }}
+                                </x-jet-dropdown-link>
+                                @endrole
+
+                            </form>
+                        </x-slot>
+
+                    </x-jet-dropdown>
+                    @else
+
+                        <x-jet-dropdown align="right" width="48">
+                            <x-slot name="trigger">
+                                <i class="fas fa-user-circle text-white text-3xl cursor-pointer"></i>
+                            </x-slot>
+                            <x-slot name="content">
+                                <x-jet-dropdown-link href="{{ route('login') }}">
+                                    {{ __('Login') }}
+                                </x-jet-dropdown-link>
+                                <x-jet-dropdown-link href="{{ route('register') }}">
+                                    {{ __('Register') }}
+                                </x-jet-dropdown-link>
+                            </x-slot>
+                        </x-jet-dropdown>
+                    @endauth
+
             </div>
         </a>
         <div class="hidden md:block">
